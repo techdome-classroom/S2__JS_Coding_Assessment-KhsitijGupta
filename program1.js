445d9b538ccef38b8b42107eaf379d37d1eb92f7
Kshitij Gupta <kshitijgupta2162@gmail.com>
2024-09-25 14:42:20 +0530

    "I have completed my code to the best of my abilities.  Thank you for the opportunity to participate in this exam."

--- program1.js
+++ program1.js
@@ -3,9 +3,30 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+ 
+    const stack = [];
     
-};
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else {
+            if (char === ')') {
+                if (stack.length === 0 || stack.pop() !== '(') {
+                    return false;
+                }
+            } else if (char === '}') {
+                if (stack.length === 0 || stack.pop() !== '{') {
+                    return false;
+                }
+            } else if (char === ']') {
+                if (stack.length === 0 || stack.pop() !== '[') {
+                    return false;
+                }
+            }
+        }
+    }
+    
+    return stack.length === 0;
+}
 
 module.exports = { isValid };
-
-
